ddb54fc6b53917956e9af551d3cb0e8053016c04
otakbeku <iceramngl@gmail.com>
2017-01-25 08:19:27 +0700

    Udah bisa semuanya. Beberapa fungsi belum di ganti.  Proses Igrasi ke SQLite

--- app/src/main/java/lemaries/religi/wazifahapp/HalamanDepan.java
+++ app/src/main/java/lemaries/religi/wazifahapp/HalamanDepan.java
@@ -1,13 +1,41 @@
 package lemaries.religi.wazifahapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HalamanDepan extends AppCompatActivity {
+    ImageButton btnPreferences;
+    Button btnSugra, btnKubra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_depan);
+
+
+        //Instansiasi
+        btnKubra = (Button) findViewById(R.id.btnKubra);
+        btnSugra = (Button) findViewById(R.id.btnSugra);
+        btnPreferences = (ImageButton) findViewById(R.id.btnPrefereces);
     }
+
+    public void intentSugra(View view) {
+        Intent intentSugra = new Intent(HalamanDepan.this, MainActivity.class);
+        intentSugra.putExtra("opsi","sugra");
+        startActivity(intentSugra);
+
+    }
+
+    public void intentKubra(View view) {
+        Intent intentKubra = new Intent(HalamanDepan.this, MainActivity.class);
+        intentKubra.putExtra("opsi","kubra");
+        startActivity(intentKubra);
+
+    }
+
+
 }
--- app/src/main/java/lemaries/religi/wazifahapp/MainActivity.java
+++ app/src/main/java/lemaries/religi/wazifahapp/MainActivity.java
@@ -1,32 +1,63 @@
 package lemaries.religi.wazifahapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textDua, textCounter;
-    int counter, duaIndex;
+    int counter = 0, duaIndex = 0;
     String[] text = {"بِسْمِ اللهِ الرَّحْمـَنِ الرَّحِيمِ الْحَمْدُ للهِ رَبِّ الْعَالَمِينَ الرَّحْمـنِ الرَّحِيمِ مَـلِكِ يَوْمِ الدِّينِ إِيَّاكَ نَعْبُدُ وإِيَّاكَ نَسْتَعِينُ اهدِنَــــا الصِّرَاطَ المُستَقِيمَ صِرَاطَ الَّذِينَ أَنعَمتَ عَلَيهِمْ غَيرِ المَغضُوبِ عَلَيهِمْ وَلاَ الضَّالِّينَ",
             "بِسْمِ اللّهِ الرَّحْمـنِ الرَّحِيمِ الـم ذَلِكَ الْكِتَابُ لاَ رَيْبَ فِيهِ هُدًى لِّلْمُتَّقِينَ الَّذِينَ يُؤْمِنُونَ بِالْغَيْبِ وَيُقِيمُونَ الصَّلاةَ وَمِمَّا رَزَقْنَاهُمْ يُنفِقُونَ والَّذِينَ يُؤْمِنُونَ بِمَا أُنزِلَ إِلَيْكَ وَمَا أُنزِلَ مِن قَبْلِكَ وَبِالآخِرَةِ هُمْ يُوقِنُونَ أُوْلَـئِكَ عَلَى هُدًى مِّن رَّبِّهِمْ وَأُوْلَـئِكَ هُمُ الْمُفْلِحُونَ",
             "nanana3"};
-    int[] text2 = {R.string.Al_Fatihah, R.string.Al_Baqarah1_5,R.string.app_name};
+    int[] text2 = {R.string.Al_Fatihah, R.string.Al_Baqarah1_5, R.string.app_name};
+    int[] textQuery;
     int[] counterSelf = {2, 2, 2};
+    int[] counterQuery;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bundle = getIntent().getExtras();
+        if (bundle != null) {
+            if (bundle.getString("opsi").equalsIgnoreCase("sugra")) {
+                //Ada 30 query
+                textQuery = new int[]{R.string.Al_Fatihah, R.string.Al_Baqarah1_5,
+                        R.string.Al_Baqarah255_257, R.string.Al_Baqarah284_286, R.string.Al_Ikhlas, R.string.Al_Falaq,
+                        R.string.An_Naas, R.string.doa_1, R.string.doa_2, R.string.doa_3, R.string.doa_4, R.string.doa_5,
+                        R.string.doa_6, R.string.doa_7, R.string.doa_8, R.string.doa_9, R.string.doa_10, R.string.doa_11,
+                        R.string.doa_12, R.string.doa_13, R.string.doa_14, R.string.doa_15, R.string.Shalawat, R.string.doa_16, R.string.doa_17,
+                        R.string.doa_18, R.string.doa_19, R.string.doa_20, R.string.Ali_Imran26_27, R.string.doa_21, R.string.doa_22};
+                counterQuery = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+            } else if (bundle.getString("opsi").equalsIgnoreCase("kubra")) {
+                //Ada 30 query
+                textQuery = new int[]{R.string.Al_Fatihah, R.string.Al_Baqarah1_5,
+                        R.string.Al_Baqarah255_257, R.string.Al_Baqarah284_286,R.string.Ali_Imran1_2,R.string.Thaaha111_112,R.string.Al_Isra110_111, R.string.Al_Muminun115_118,R.string.Ar_Ruum17_26,R.string.Al_Mukmin1_3,
+                        R.string.Al_Hasyr22_24,R.string.Al_Zalzalah,R.string.Al_kafirun,R.string.An_Nasr, R.string.Al_Ikhlas, R.string.Al_Falaq,
+                        R.string.An_Naas, R.string.doa_1, R.string.doa_2, R.string.doa_3, R.string.doa_4, R.string.doa_5,
+                        R.string.doa_6, R.string.doa_7, R.string.doa_8, R.string.doa_9, R.string.doa_10, R.string.doa_11,
+                        R.string.doa_12, R.string.doa_13, R.string.doa_14, R.string.doa_15, R.string.Shalawat, R.string.doa_16, R.string.doa_17,
+                        R.string.doa_18, R.string.doa_19, R.string.doa_20, R.string.Ali_Imran26_27, R.string.doa_21, R.string.doa_22};
+                counterQuery = new int[]{1, 1, 1, 1,  1,1,7,1,1,1,1,1,1,1,1,  3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 10, 100, 10, 1, 1, 1, 1, 1, 1};
+            }
+        }
+
 
         //Instansiasi Content
         textDua = (TextView) findViewById(R.id.textDua);
         textCounter = (TextView) findViewById(R.id.textCounter);
 
-        counter = 0;
-        duaIndex = 0;
+        ///Ubah font
+
+        Typeface font_arab = Typeface.createFromAsset(getAssets(),"me_quran.ttf");
+        textDua.setTypeface(font_arab);
 
         //Set
         textCounter.setText("");
@@ -39,17 +70,34 @@ public class MainActivity extends AppCompatActivity {
             Log.i("==:", "========================");
             Log.i("nilai duaIndex:", Integer.toString(duaIndex));
             Log.i("nilai counter:", Integer.toString(counter));
-            if (counter < counterSelf[duaIndex]) {
+            Log.i("panjangA", "" + counterQuery.length);
+//            if (counter < counterSelf[duaIndex]) {
+////                textDua.setText(text[duaIndex]);
+//                textDua.setText(text2[duaIndex]);
+//                textCounter.setText("x" + (counter + 1));
+//                counter++;
+//                Log.i("Dalam counter:", Integer.toString(counter));
+//            } else if (counter == counterSelf[duaIndex]) {
+//                textCounter.setText("x" + (counter + 1));
+//                counter = 0;
+//                ++duaIndex;
+//            }
+            if (counter < counterQuery[duaIndex]) {
 //                textDua.setText(text[duaIndex]);
-                textDua.setText(text2[duaIndex]);
-                textCounter.setText("x" + (counter+1));
+                textDua.setText(textQuery[duaIndex]);
+                textCounter.setText("x" + (counter + 1));
                 counter++;
                 Log.i("Dalam counter:", Integer.toString(counter));
-            } else if (counter == counterSelf[duaIndex]) {
-                textCounter.setText("x" + (counter+1));
-                counter = 0;
+            } else if (duaIndex + 1 == counterQuery.length) {
+                Toast.makeText(getApplicationContext(), "Anda sudah diujung", Toast.LENGTH_SHORT).show();
+            } else if (counter == counterQuery[duaIndex]) {
+//                textCounter.setText("x" + (counter + 1));
+                textCounter.setText("");
+                textDua.setText(textQuery[duaIndex + 1]);
+                counter = 1;
                 ++duaIndex;
             }
+
         } catch (Exception e) {
             Log.i("Error:", "nanana joker: " + e.toString());
         }
